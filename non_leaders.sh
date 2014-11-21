983ac6421c5b3df11d1c85ea352b3c68ea9eb4ed
vishakm92 <vishakm92@gmail.com>
2014-11-20 20:10:19 -0800

    working shell script

--- non_leaders.sh
+++ non_leaders.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 count=1
-#while [ $count -le $1 ]
-#do
-echo "Welcone $count times"
-port=5000+$count
-echo port
-gnome-terminal -x "python server_non_leader.py 5000"
-#count=$(( count+1 ))	 # increments $n
-#done
+
+while [ $count -le $1 ]
+do
+	echo "Welcone $count times"
+	port=$(( count+5000))
+	echo $port
+	#cmd+=( --tab -e "python server_non_leader.py 5000" )
+	#x-terminal-emulator -e python server_non_leader.py 5000 
+	gnome-terminal --tab -e "bash -c \"python server_non_leader.py $port; exec bash\""
+	count=$(( count+1 ))	 # increments $n
+done
